7ae1fb7b731016dfef787d3e04b879f27e0280fb
root <kooky06@outlook.com>
2019-05-23 13:22:35 +0000

    first commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
       public static void main (String [] args )
       {
-            System.out.println("Hello CENG");
+            System.out.println("Hello the New World of CENG");
       }
 }
